6e997baff37aa38ffcb0aaf8af7c1d9d1cb7690f
steadfasterX <steadfasterx@binbash.rocks>
2025-08-14 07:57:56 +0000

    bump version 1.0.0_AOS-3/1008

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,8 +41,8 @@ android {
         targetSdk = 33
         applicationId = "com.saggitt.omega"
 
-        versionName = "1.0.0_AOS-2"
-        versionCode = 1007
+        versionName = "1.0.0_AOS-3"
+        versionCode = 1008
 
         buildConfigField("String", "BUILD_DATE", "\"${getBuildDate()}\"")
         buildConfigField("boolean", "ENABLE_AUTO_INSTALLS_LAYOUT", "false")
